13448b4c5ba91810c90ccc4b5d8cc0e3bd327fe3
CasuallyDreamin <ysn.arambash@gmail.com>
2026-01-06 11:09:28 -0500

    vercel deployment conf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://odin-blog-api-i8n5.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
